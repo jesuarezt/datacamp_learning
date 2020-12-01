e6c8a9b203cc0ffcce344a5c4fe459a041327eac
jesuarezt <71199932+jesuarezt@users.noreply.github.com>
2020-12-01 18:51:30 -0500

    Update chapter4.py

--- python_career_machine_learning_scientist/16_Introduction_to_Deep_Learning_with_keras/chapter4.py
+++ python_career_machine_learning_scientist/16_Introduction_to_Deep_Learning_with_keras/chapter4.py
@@ -293,3 +293,38 @@ model.summary()
 
 
 
+def predict_text(test_text, model = model):
+  if len(test_text.split()) != 3:
+    print('Text input should be 3 words!')
+    return False
+  
+  # Turn the test_text into a sequence of numbers
+  test_seq = tokenizer.texts_to_sequences([test_text])
+  test_seq = np.array(test_seq)
+  
+  # Use the model passed as a parameter to predict the next word
+  pred = model.predict(test_seq).argmax(axis = 1)[0]
+  
+  # Return the word that maps to the prediction
+  return tokenizer.index_word[pred]
+
+#Test your model!
+#
+#The function you just built, predict_text(), is ready to use. Remember that the model object is already passed by default as the second parameter so you just need to provide the function with your 3 word sentences.
+#
+#Try out these strings on your LSTM model:
+#
+#    'meet revenge with'
+#    'the course of'
+#    'strength of the'
+#
+#Which sentence could be made with the word output from the sentences above?
+
+print(predict_text('meet revenge with', model= model))
+print(predict_text('the course of', model= model))
+print(predict_text('strength of the', model= model))
+
+
+
+
+#Revenge is your history and spirit
